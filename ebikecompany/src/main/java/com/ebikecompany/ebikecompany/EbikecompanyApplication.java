1499c842bf081f408c4de2a9dc9f7f61391bb5a7
thiagobfernandes <thiago5437br@gmail.com>
2024-11-06 21:40:00 -0300

    Banco de Dados e estrutura do projeto

---
+++ ebikecompany/src/main/java/com/ebikecompany/ebikecompany/EbikecompanyApplication.java
@@ -0,0 +1,13 @@
+package com.ebikecompany.ebikecompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbikecompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbikecompanyApplication.class, args);
+	}
+
+}
---
+++ ebikecompany/src/main/resources/db/migration/V2__create-table-categorias.sql
@@ -0,0 +1,4 @@
+CREATE TABLE categorias(
+    id_categoria INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL
+);
